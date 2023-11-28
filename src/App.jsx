d8d1c7c5d1953da791b06354c21df6806a25605b
yasinciftcii <yasinciftci_32@outlook.com>
2023-11-28 19:43:05 +0300

    pomodoro app

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import "./App.css";
+import Timer from "./Timer";
+import Settings from "./Settings";
+import { useState } from "react";
+import SettingsContext from "./SettingsContext";
+
+function App() {
+  const [showSettings, setShowSettings] = useState(false);
+  const [workMinutes, setWorkMinutes] = useState(25);
+  const [breakMinutes, setBreakMinutes] = useState(10);
+
+  return (
+    <main>
+      <SettingsContext.Provider
+        value={{
+          showSettings,
+          setShowSettings,
+          workMinutes,
+          breakMinutes,
+          setWorkMinutes,
+          setBreakMinutes,
+        }}
+      >
+        {showSettings ? <Settings /> : <Timer />}
+      </SettingsContext.Provider>
+    </main>
+  );
+}
+
+export default App;
